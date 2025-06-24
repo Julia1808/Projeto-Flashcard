0890464aaca484ae91ab41e189baf0cc7fa8ef0d
Julia1808 <julia.toyomoto@escola.pr.gov.br>
2025-06-24 02:02:57 +0000

    alterações no projeto

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'O que foi a Segunda Guerra Mundial?',
+    'A Segunda Guerra Mundial foi um conflito global que ocorreu de 1939 à 1945, sendo causada pelo descontentamento alemão com o Tratado de Veersalhes e o crescente expansionismo das nações'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é a diferença entre briófitas e pterodófitas?',
+    'As pterodófitas, diferente das briófitas, possuem orgãos verdadeiros e vasos condutores de seiva que ajudam no transporte de água e nutrientes'
+)
+
+criaCartao(
+    'Matemática',
+    'O que é um polígono?',
+    'É uma figura geométrica plana e fechada, formada por segmentos de reta que não se cruzam'
+)
+
+criaCartao(
+    'Química'
+    
+
+)
